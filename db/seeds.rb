1aaa8c59d268d2b53088481aa10beb468819649b
Hitoshi Imaizumi <ima2251@users.noreply.github.com>
2022-06-27 14:46:05 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+comment = Comment.create([
+  { body: "test comment 1" },
+  { body: "test comment 2", parent_id: 1 }
+])
